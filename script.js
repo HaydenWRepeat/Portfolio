05019236192c962189e987cea73538af2548b563
HaydenW <gamerwiechens35@outlook.com>
2021-03-10 18:24:44 -0700

    fixed code

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 'use script'
 
 $(function(){
-    $("#resume-selector").children().on("click", function() {
-        $(`${$(this).attr('class')}-content`).css({
-            opacity: 1
-        })
+
+    $(`.resume .resume-selector div`).on(`click`, function(){
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~TABS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+        //remove the active from the old one
+        $(".resume").find(`.resume-selector div.active`).removeClass(`active`);
+        //add the active class tot he li that you clicked on
+        $(this).addClass(`active`)
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PANELS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+        
+        // figure out which panel we want to show
+        let $resumeConetentShow = $(this).attr(`rel`);
+
+        //hide the current panel
+        $(".resume").find(`.resume-content .active`).slideUp(300, showResumeContent);
+        
+        //find the next and display it
+        function showResumeContent(){
+            $(this).removeClass(`active`);
+            $(`#${$resumeConetentShow}`).show(300, function(){
+                $(this).addClass(`active`);
+            })
+        }
     })
 })
